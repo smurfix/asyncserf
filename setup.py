ab3f39ab894df5160b3f06d0cb6e2dc0d5137416
Matthias Urlichs <matthias@urlichs.de>
2018-12-19 10:20:12 +0100

    Add explicit dependency on attr >= 18.1

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ setup(
     keywords=['Serf', 'orchestration', 'service discovery', 'anyio'],
     license='MIT',
     packages=['aioserf'],
-    install_requires=['msgpack >= 0.5.0', 'anyio', 'outcome', 'async_generator'],
+    install_requires=['msgpack >= 0.5.0', 'anyio', 'outcome', 'async_generator', 'attr >= 18.1'],
     tests_require=test_requires,
     cmdclass={'test': PyTest},
 )
